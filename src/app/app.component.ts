b820f1f4a45dbb2857d7a083aaedb75014318e0a
Leonidas810 <leonardo20010912@gmail.com>
2024-04-08 22:32:00 -0600

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [NavbarComponent]
+    imports: [NavbarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'finalproyect';
